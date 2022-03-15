9c8563d6933c79e88f7e3121755eeed023d1e662
机械师 <lydxh935227514@gmail.com>
2022-03-16 01:27:00 +0800

    更新CPU与Mem占用显示

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/adlane/exec v0.0.0-20171006170040-bf2ccecd9b4d
+	github.com/shirou/gopsutil v2.21.11+incompatible
 	github.com/spf13/viper v1.10.0
 	github.com/tidwall/gjson v1.12.1
 	github.com/vbauerster/mpb/v7 v7.2.0
@@ -15,6 +16,7 @@ require (
 	github.com/VividCortex/ewma v1.2.0 // indirect
 	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
@@ -28,7 +30,10 @@ require (
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
-	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
+	github.com/tklauser/go-sysconf v0.3.9 // indirect
+	github.com/tklauser/numcpus v0.3.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/adlane/exec"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
 	"golang.org/x/text/encoding/simplifiedchinese"
 	"os"
 	"os/signal"
 	"runtime"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
@@ -26,7 +29,62 @@ var r reader
 var cmd string
 var font string
 var arg [4]string
+var info [2]string
+var Debug bool = true
+var (
+	kernel32    *syscall.LazyDLL  = syscall.NewLazyDLL(`kernel32.dll`)
+	proc        *syscall.LazyProc = kernel32.NewProc(`SetConsoleTextAttribute`)
+	CloseHandle *syscall.LazyProc = kernel32.NewProc(`CloseHandle`)
 
+	// FontC 给字体颜色对象赋值
+	FontC Color = Color{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+)
+
+type Color struct {
+	black        int // 黑色
+	blue         int // 蓝色
+	green        int // 绿色
+	cyan         int // 青色
+	red          int // 红色
+	purple       int // 紫色
+	yellow       int // 黄色
+	light_gray   int // 淡灰色（系统默认值）
+	gray         int // 灰色
+	light_blue   int // 亮蓝色
+	light_green  int // 亮绿色
+	light_cyan   int // 亮青色
+	light_red    int // 亮红色
+	light_purple int // 亮紫色
+	light_yellow int // 亮黄色
+	white        int // 白色
+}
+
+const (
+	_  = iota
+	KB = 1 << (10 * iota)
+	MB = 1 << (10 * iota)
+	GB = 1 << (10 * iota)
+	TB = 1 << (10 * iota)
+	PB = 1 << (10 * iota)
+)
+
+func GetCpu() string {
+	percent, _ := cpu.Percent(time.Second, false)
+	return "C:" + strconv.FormatFloat(percent[0], 'f', 1, 64) + "%"
+}
+
+func GetMem() string {
+	v, _ := mem.VirtualMemory()
+	return "M:" + strconv.FormatFloat(v.UsedPercent, 'f', 1, 64) + "%)"
+}
+func CPrint(s string, i int) {
+	handle, _, _ := proc.Call(uintptr(syscall.Stdout), uintptr(i))
+	fmt.Print(s)
+	_, _, err := CloseHandle.Call(handle)
+	if err != nil {
+		return
+	}
+}
 func ConvertByte2String(byte []byte, charset Charset) string {
 	var str string
 	switch charset {
@@ -42,49 +100,50 @@ func ConvertByte2String(byte []byte, charset Charset) string {
 }
 func init() {
 	fmt.Println("欢迎使用BE6-CLOUD一键开服器")
-	var config tools.Config
-	err := os.Chdir("./")
-	err = os.MkdirAll("./config/mods", 0777)
-	err = os.MkdirAll("./config/scripts", 0777)
-	err = os.MkdirAll("./config/scripts/cache", 0777)
-	config = tools.GetConfig()
-	MVersion, MDownUrl := tools.MGetVersion(config.MindustryTagUrl)
-	WVersion, WJarUrl, WZipUrl, WCaAUrl := tools.WGetVersion(config.WayZerTagUrl)
-	fmt.Println("Mindustry当前版本：", config.MindustryVersion)
-	fmt.Println("WayZer当前版本：", config.WayZerVersion)
-	DownList := tools.NewDownloader("./")
-	DownList.Concurrent = 3
-	var Down bool
-	var Mode = false
-	if config.MindustryVersion != MVersion {
-		fmt.Println("Mindustry最新版本：", MVersion, "有更新")
-		config.MindustryVersion = MVersion
-		DownList.AppendResource("server.jar", MDownUrl)
-		Down = true
-	}
-	if config.WayZerVersion != WVersion {
-		fmt.Println("WayZer最新版本：", WVersion, "有更新")
-		config.WayZerVersion = WVersion
-
-		DownList.AppendResource("WayZer.jar", WJarUrl)
-		DownList.AppendResource("WayZer.zip", WZipUrl)
-		DownList.AppendResource("Cache.zip", WCaAUrl)
-
-		Down = true
-		Mode = true
-	}
-	if Down {
-		err = DownList.Start()
-		tools.SaveConfig(config)
-		if Mode {
-			err = os.Rename("./WayZer.jar", "./config/mods/WayZer.jar")
-			err = tools.DeCompressZip("./WayZer.zip", "./config/scripts")
-			err = tools.DeCompressZip("./Cache.zip", "./config/scripts/cache")
-
+	if !Debug {
+		var config tools.Config
+		err := os.Chdir("./")
+		err = os.MkdirAll("./config/mods", 0777)
+		err = os.MkdirAll("./config/scripts", 0777)
+		err = os.MkdirAll("./config/scripts/cache", 0777)
+		config = tools.GetConfig()
+		MVersion, MDownUrl := tools.MGetVersion(config.MindustryTagUrl)
+		WVersion, WJarUrl, WZipUrl, WCaAUrl := tools.WGetVersion(config.WayZerTagUrl)
+		fmt.Println("Mindustry当前版本：", config.MindustryVersion)
+		fmt.Println("WayZer当前版本：", config.WayZerVersion)
+		DownList := tools.NewDownloader("./")
+		DownList.Concurrent = 3
+		var Down bool
+		var Mode = false
+		if config.MindustryVersion != MVersion {
+			fmt.Println("Mindustry最新版本：", MVersion, "有更新")
+			config.MindustryVersion = MVersion
+			DownList.AppendResource("server.jar", MDownUrl)
+			Down = true
 		}
-	}
-	if err != nil {
-		return
+		if config.WayZerVersion != WVersion {
+			fmt.Println("WayZer最新版本：", WVersion, "有更新")
+			config.WayZerVersion = WVersion
+
+			DownList.AppendResource("WayZer.jar", WJarUrl)
+			DownList.AppendResource("WayZer.zip", WZipUrl)
+			DownList.AppendResource("Cache.zip", WCaAUrl)
+
+			Down = true
+			Mode = true
+		}
+		if Down {
+			err = DownList.Start()
+			tools.SaveConfig(config)
+			if Mode {
+				err = os.Rename("./WayZer.jar", "./config/mods/WayZer.jar")
+				err = tools.DeCompressZip("./WayZer.zip", "./config/scripts")
+				err = tools.DeCompressZip("./Cache.zip", "./config/scripts/cache")
+			}
+		}
+		if err != nil {
+			return
+		}
 	}
 }
 func main() {
@@ -108,12 +167,14 @@ func main() {
 	ctx = exec.InteractiveExec(cmd, arg[0], arg[1], arg[2], arg[3], "-jar", "./server.jar")
 
 	go ctx.Receive(&r, 10*time.Second)
+	info[0] = GetCpu()
+	info[1] = GetMem()
 	input := bufio.NewScanner(os.Stdin)
 	var cmds = [...]string{".help", ".restart", ".exit"}
 	var deps = [...]string{"帮助信息", "重新启动服务端", "退出启动器"}
 	go func() {
 		for {
-			fmt.Print("\r> ")
+			CPrint("\r"+info[0]+info[1]+"> ", FontC.light_green)
 			input.Scan()
 			if strings.Compare(strings.TrimSpace(input.Text()), "") == 0 {
 				continue
@@ -153,15 +214,66 @@ type reader struct {
 }
 
 func (*reader) OnData(b []byte) bool {
-	fmt.Print(ConvertByte2String(b, Charset(font)))
-	return false
+	Msg := ConvertByte2String(b, Charset(font))
+	if runtime.GOOS == "windows" {
+		if find := strings.Contains(Msg, "I"); find {
+			//fmt.Printf("\x1b[%dm%v\x1b[0m", 34, Msg)
+			if find := strings.Contains(Msg, "* "); find {
+				//fmt.Printf("\x1b[%dm%v\x1b[0m", 33, Msg)
+				CPrint(Msg, FontC.light_purple)
+				return false
+			}
+			CPrint(Msg, FontC.light_cyan)
+			return false
+		}
+		if find := strings.Contains(Msg, "E"); find {
+			//fmt.Printf("\x1b[%dm%v\x1b[0m", 33, Msg)
+			CPrint(Msg, FontC.red)
+			return false
+		}
+		CPrint(Msg, FontC.white)
+		return false
+	} else {
+		fmt.Print(Msg)
+		return false
+	}
 }
 
 func (*reader) OnError(b []byte) bool {
-	fmt.Print(ConvertByte2String(b, Charset(font)))
-	return false
+	Msg := ConvertByte2String(b, Charset(font))
+	if runtime.GOOS == "windows" {
+		if find := strings.Contains(Msg, " 警告"); find {
+			//fmt.Printf("\x1b[%dm%v\x1b[0m", 32, Msg)
+			CPrint(Msg, FontC.purple)
+			return false
+		}
+		if find := strings.Contains(Msg, "INFO"); find {
+			//fmt.Printf("\x1b[%dm%v\x1b[0m", 34, Msg)
+			CPrint(Msg, FontC.light_cyan)
+			return false
+		}
+		if find := strings.Contains(Msg, "信息"); find {
+			//fmt.Printf("\x1b[%dm%v\x1b[0m", 32, Msg)
+			CPrint(Msg, FontC.light_blue)
+			return false
+		}
+
+		if find := strings.Contains(Msg, "WARN"); find {
+			//fmt.Printf("\x1b[%dm%v\x1b[0m", 35, Msg)
+			CPrint(Msg, FontC.purple)
+			return false
+		}
+		CPrint(Msg, FontC.white)
+		return false
+	} else {
+		fmt.Print(Msg)
+		return false
+	}
 }
 
 func (*reader) OnTimeout() {
-	go ctx.Receive(&r, 10*time.Second)
+	go ctx.Receive(&r, 5*time.Second)
+	info[0] = GetCpu()
+	info[1] = GetMem()
+
 }
